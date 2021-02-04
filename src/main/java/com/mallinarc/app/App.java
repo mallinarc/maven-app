1ccb2a90dc6d9938dce5a82e6a7c528e26dbab5a
mallinarc <mallinarc@hotmail.com>
2021-02-04 16:35:22 -0500

    initial commit

---
+++ src/main/java/com/mallinarc/app/App.java
@@ -0,0 +1,13 @@
+package com.mallinarc.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
